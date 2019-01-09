Fixing bug when calcluate the average motor speed.

procedures/adjustMotorOntime.py
@@ -64,11 +64,18 @@ class adjustOnTime():
         j2fwd_avg = np.zeros(size)
         j2rev_avg = np.zeros(size)
 
+        
         for i in range(size):
-            j1fwd_avg[i] = np.mean(np.rad2deg(model.angularSteps[i]/model.S1Pm[i]))
-            j1rev_avg[i] = -np.mean(np.rad2deg(model.angularSteps[i]/model.S1Nm[i]))
-            j2fwd_avg[i] = np.mean(np.rad2deg(model.angularSteps[i]/model.S2Pm[i]))
-            j2rev_avg[i] = -np.mean(np.rad2deg(model.angularSteps[i]/model.S2Nm[i]))
+            
+            # Calculate the limit index
+            j1limit = 360/np.rad2deg(model.angularSteps[i])
+            j2limit = 180/np.rad2deg(model.angularSteps[i])
+
+            # The average should be in the range of 360 for theta, 180 for phi 
+            j1fwd_avg[i] = np.mean(np.rad2deg(model.angularSteps[i]/model.S1Pm[i][:j1limit.astype(int)-1]))
+            j1rev_avg[i] = -np.mean(np.rad2deg(model.angularSteps[i]/model.S1Nm[i][:j1limit.astype(int)-1]))
+            j2fwd_avg[i] = np.mean(np.rad2deg(model.angularSteps[i]/model.S2Pm[i][:j2limit.astype(int)-1]))
+            j2rev_avg[i] = -np.mean(np.rad2deg(model.angularSteps[i]/model.S2Nm[i][:j2limit.astype(int)-1]))
 
         otm = ontimeModel()
         newOntimeFwd1 = otm.getTargetOnTime(0.05,otm.j1fwd_slope, model.motorOntimeFwd1 ,j1fwd_avg)

procedures/checkMotorMap.py
@@ -79,15 +79,16 @@ def readMotorMap(xml,pid):
 def main():
     dataPath='/Volumes/Disk/Data/xml/'
     xml1=dataPath+'motormaps_181205.xml'
+    xml2=dataPath+f'motormap20190109.xml'
     figpath='/Volumes/Disk/Data/MotorMap/20190109/'
     
     brokens = [1 , 12, 39, 43, 54]
     visibles= [e for e in range(1,58) if e not in brokens]
     #visibles = [2,3,4]
 
-    mapper = d3['Category20'][10]
+    model1=extractCalibModel(xml1)
+    model2=extractCalibModel(xml2)
 
-  
     for i in visibles:
     
         pid=i
@@ -102,14 +103,6 @@ def main():
 
         q.xaxis.axis_label = "Degree"
         q.yaxis.axis_label = "Speed"
-        colorcode = 0
-        #for tms in range(25,90,10):
-
-        xml2=dataPath+f'motormap20190109.xml'
-        print(xml2)
-        
-        model1=extractCalibModel(xml1)
-        model2=extractCalibModel(xml2)
 
         legendname="Ontime = "+str(model2.motorOntimeFwd1[pid-1])
 
@@ -117,37 +110,46 @@ def main():
         #if not (os.path.exists(mappath)):
         #    os.makedirs(mappath)
 
-        
+        # calculate the limiting index for motor map
+        j1limit1 = (360/np.rad2deg(model1.angularSteps[pid-1])).astype(int)-1
+        j2limit1 = (180/np.rad2deg(model1.angularSteps[pid-1])).astype(int)-1
 
+        j1limit2 = (360/np.rad2deg(model2.angularSteps[pid-1])).astype(int)-1
+        j2limit2 = (180/np.rad2deg(model2.angularSteps[pid-1])).astype(int)-1
+
+        print(j1limit1,j2limit1,j1limit2,j2limit2)
         j1_fwd_reg1,j1_fwd_stepsize1,j1_rev_reg1,j1_rev_stepsize1,\
                 j2_fwd_reg1,j2_fwd_stepsize1,j2_rev_reg1,j2_rev_stepsize1=readMotorMap(xml1,pid)
         j1_fwd_reg2,j1_fwd_stepsize2,j1_rev_reg2,j1_rev_stepsize2,\
                 j2_fwd_reg2,j2_fwd_stepsize2,j2_rev_reg2,j2_rev_stepsize2=readMotorMap(xml2,pid)
-        legendname=f"Avg Speed = {np.mean(j1_fwd_stepsize1):.4f} Ontime = {model1.motorOntimeFwd1[i-1]:.4f}"
-        p.line(x=j1_fwd_reg1, y=j1_fwd_stepsize1, color='green', line_width=2, legend=legendname)
+        legendname=f"Avg Speed = {np.mean(j1_fwd_stepsize1[:j1limit1]):.4f} Ontime = {model1.motorOntimeFwd1[pid-1]:.4f}"
+        p.line(x=j1_fwd_reg1[:j1limit1], y=j1_fwd_stepsize1[:j1limit1], color='green', line_width=2, legend=legendname)
         
-        legendname=f"Avg Speed = {np.mean(j1_fwd_stepsize2):.4f} Ontime = {model2.motorOntimeFwd1[i-1]:.4f}"
-        p.line(x=j1_fwd_reg2, y=j1_fwd_stepsize2, color='red', line_width=2,legend=legendname)
+        legendname=f"Avg Speed = {np.mean(j1_fwd_stepsize2[:j1limit2]):.4f} Ontime = {model2.motorOntimeFwd1[pid-1]:.4f}"
+        p.line(x=j1_fwd_reg2[:j1limit2], y=j1_fwd_stepsize2[:j1limit2], color='red', line_width=2,legend=legendname)
+       
         #p.circle(x=j1_fwd_reg2, y=j1_fwd_stepsize2,radius=1, color=mapper[colorcode],fill_color='white')
-        legendname=f"Avg Speed = {np.mean(j1_rev_stepsize1):.4f} Ontime = {model1.motorOntimeRev1[i-1]:.4f}"
-        p.line(x=j1_rev_reg1, y=j1_rev_stepsize1, color='green', line_width=2,line_dash="4 4", legend=legendname)
-        legendname=f"Avg Speed = {np.mean(j1_rev_stepsize2):.4f} Ontime = {model2.motorOntimeRev1[i-1]:.4f}"
-        p.line(x=j1_rev_reg2, y=j1_rev_stepsize2, color='red', line_width=2,line_dash="4 4",legend=legendname)
+        legendname=f"Avg Speed = {np.mean(j1_rev_stepsize1[:j1limit1]):.4f} Ontime = {model1.motorOntimeRev1[pid-1]:.4f}"
+        p.line(x=j1_rev_reg1[:j1limit1], y=j1_rev_stepsize1[:j1limit1], color='green', line_width=2,line_dash="4 4", legend=legendname)
+       
+        legendname=f"Avg Speed = {np.mean(j1_rev_stepsize2[:j1limit2]):.4f} Ontime = {model2.motorOntimeRev1[pid-1]:.4f}"
+        p.line(x=j1_rev_reg2[:j1limit2], y=j1_rev_stepsize2[:j1limit2], color='red', line_width=2,line_dash="4 4",legend=legendname)
         #p.circle(x=j1_rev_reg2, y=j1_rev_stepsize2,radius=1, color=mapper[colorcode],fill_color='white')
 
-        legendname=f"Avg Speed = {np.mean(j2_fwd_stepsize1):.4f} Ontime = {model1.motorOntimeFwd2[i-1]:0.4f}"
-        q.line(x=j2_fwd_reg1, y=j2_fwd_stepsize1, color='green', line_width=3, legend=legendname)
-        legendname=f"Avg Speed = {np.mean(j2_fwd_stepsize2):.4f} Ontime = {model2.motorOntimeFwd2[i-1]:0.4f}"
-        q.line(x=j2_fwd_reg2, y=j2_fwd_stepsize2, color='red', line_width=2,legend=legendname)
+        legendname=f"Avg Speed = {np.mean(j2_fwd_stepsize1[:j2limit1]):.4f} Ontime = {model1.motorOntimeFwd2[pid-1]:0.4f}"
+        q.line(x=j2_fwd_reg1[:j2limit1], y=j2_fwd_stepsize1[:j2limit1], color='green', line_width=3, legend=legendname)
+        
+        legendname=f"Avg Speed = {np.mean(j2_fwd_stepsize2[:j2limit2]):.4f} Ontime = {model2.motorOntimeFwd2[pid-1]:0.4f}"
+        q.line(x=j2_fwd_reg2[:j2limit2], y=j2_fwd_stepsize2[:j2limit2], color='red', line_width=2,legend=legendname)
         #q.circle(x=j2_fwd_reg2, y=j2_fwd_stepsize2,radius=1, color='red', fill_color='white')
         
-        legendname=f"Avg Speed = {np.mean(j2_rev_stepsize1):.4f} Ontime = {model1.motorOntimeRev2[i-1]:.4f}"
-        q.line(x=j2_rev_reg1, y=j2_rev_stepsize1, color='green', line_width=2,line_dash="4 4", legend=legendname)
-        legendname=f"Avg Speed = {np.mean(j2_rev_stepsize2):.4f} Ontime = {model2.motorOntimeRev2[i-1]:.4f}"
-        q.line(x=j2_rev_reg2, y=j2_rev_stepsize2, color='red', line_width=2,line_dash="4 4",legend=legendname)
+        legendname=f"Avg Speed = {np.mean(j2_rev_stepsize1[:j2limit1]):.4f} Ontime = {model1.motorOntimeRev2[pid-1]:.4f}"
+        q.line(x=j2_rev_reg1[:j2limit1], y=j2_rev_stepsize1[:j2limit1], color='green', line_width=2,line_dash="4 4", legend=legendname)
+        
+        legendname=f"Avg Speed = {np.mean(j2_rev_stepsize2[:j2limit2]):.4f} Ontime = {model2.motorOntimeRev2[pid-1]:.4f}"
+        q.line(x=j2_rev_reg2[:j2limit2], y=j2_rev_stepsize2[:j2limit2], color='red', line_width=2,line_dash="4 4",legend=legendname)
         #q.circle(x=j2_rev_reg2, y=j2_rev_stepsize2,radius=1, color='red', fill_color='white')
 
-        colorcode = colorcode+1
 
         #show(column(p,q))
         export_png(column(p,q),filename=figpath+"motormap_"+str(int(pid))+".png")