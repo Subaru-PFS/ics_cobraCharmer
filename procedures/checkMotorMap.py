8f21c46732eecf19f00a87b7a6b740ce23be06cb
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2019-01-10 14:41:35 +0800

    Revert motor map tool

--- procedures/checkMotorMap.py
+++ procedures/checkMotorMap.py
@@ -13,7 +13,6 @@ import bokeh.palettes
 from bokeh.layouts import column,gridplot
 from bokeh.models import HoverTool, ColumnDataSource, LinearColorMapper
 from bokeh.models.glyphs import Text
-from bokeh.palettes import d3
 
 from bokeh.transform import linear_cmap
 
@@ -74,84 +73,95 @@ def readMotorMap(xml,pid):
     return j1_fwd_reg,j1_fwd_stepsize,j1_rev_reg,j1_rev_stepsize,\
            j2_fwd_reg,j2_fwd_stepsize,j2_rev_reg,j2_rev_stepsize
 
+def generateMotorMap(baseXML, newXML, figPath, fiberlist = False):
+    xml1 = baseXML 
+    xml2 = newXML
 
-
-def main():
-    dataPath='/home/pfs/mhs/devel/ics_cobraCharmer.cwen/xml/'
-    xml1=dataPath+'motormaps_181205.xml'
-    figpath='/dada/pfs/MotorMap/20190109/'
+    model1 = extractCalibModel(xml1)
+    model2 = extractCalibModel(xml2)
     
-    brokens = [1 , 12, 39, 43, 54]
-    visibles= [e for e in range(1,58) if e not in brokens]
-    #visibles = [2,3,4]
+    if fiberlist is not False:
+        visibles = fiberlist
+    else:
+        visibles = range(57)
 
-    mapper = d3['Category20'][10]
-
-  
     for i in visibles:
     
         pid=i
         TOOLS = ['pan','box_zoom','wheel_zoom', 'save' ,'reset','hover']
 
-        p = figure(tools=TOOLS, x_range=[0, 600], y_range=[-0.2,0.2],plot_height=400,
+        p = figure(tools=TOOLS, x_range=[0, 550], y_range=[-0.2,0.2],plot_height=400,
                 plot_width=1000,title="Fiber No. "+str(int(pid)))
 
         p.yaxis.axis_label = "Speed"
 
-        q = figure(tools=TOOLS, x_range=[0, 400], y_range=[-0.3,0.3],plot_height=400,plot_width=545)
+        q = figure(tools=TOOLS, x_range=[0, 300], y_range=[-0.3,0.3],plot_height=400,plot_width=1000)
 
         q.xaxis.axis_label = "Degree"
         q.yaxis.axis_label = "Speed"
-        colorcode = 0
-        #for tms in range(25,90,10):
-        tms = 10
-
-        xml2=dataPath+f'motormap20190107.xml'
-        print(xml2)
-        
-        model1=extractCalibModel(xml1)
-        model2=extractCalibModel(xml2)
 
         legendname="Ontime = "+str(model2.motorOntimeFwd1[pid-1])
 
         # Prepare the data path for the work
-        #if not (os.path.exists(mappath)):
-        #    os.makedirs(mappath)
+        if not (os.path.exists(figPath)):
+            os.makedirs(figPath)
 
-        
+        # calculate the limiting index for motor map
+        j1limit1 = (360/np.rad2deg(model1.angularSteps[pid-1])).astype(int)-1
+        j2limit1 = (180/np.rad2deg(model1.angularSteps[pid-1])).astype(int)-1
+
+        j1limit2 = (360/np.rad2deg(model2.angularSteps[pid-1])).astype(int)-1
+        j2limit2 = (180/np.rad2deg(model2.angularSteps[pid-1])).astype(int)-1
 
         j1_fwd_reg1,j1_fwd_stepsize1,j1_rev_reg1,j1_rev_stepsize1,\
                 j2_fwd_reg1,j2_fwd_stepsize1,j2_rev_reg1,j2_rev_stepsize1=readMotorMap(xml1,pid)
         j1_fwd_reg2,j1_fwd_stepsize2,j1_rev_reg2,j1_rev_stepsize2,\
                 j2_fwd_reg2,j2_fwd_stepsize2,j2_rev_reg2,j2_rev_stepsize2=readMotorMap(xml2,pid)
-        legendname=f"Avg Speed = {np.mean(j1_fwd_stepsize1):.4f} Ontime = {model1.motorOntimeFwd1[i-1]:.4f}"
-        p.line(x=j1_fwd_reg1, y=j1_fwd_stepsize1, color='green', line_width=2, legend=legendname)
+        legendname=f"Avg Speed = {np.mean(j1_fwd_stepsize1[:j1limit1]):.4f} Ontime = {model1.motorOntimeFwd1[pid-1]:.4f}"
+        p.line(x=j1_fwd_reg1[:j1limit1], y=j1_fwd_stepsize1[:j1limit1], color='green', line_width=2, legend=legendname)
         
-        legendname=f"Avg Speed = {np.mean(j1_fwd_stepsize2):.4f} Ontime = {model2.motorOntimeFwd1[i-1]:.4f}"
-        p.line(x=j1_fwd_reg2, y=j1_fwd_stepsize2, color='red', line_width=2,legend=legendname)
-        #p.circle(x=j1_fwd_reg2, y=j1_fwd_stepsize2,radius=1, color=mapper[colorcode],fill_color='white')
-        legendname=f"Avg Speed = {np.mean(j1_rev_stepsize1):.4f} Ontime = {model1.motorOntimeRev1[i-1]:.4f}"
-        p.line(x=j1_rev_reg1, y=j1_rev_stepsize1, color='green', line_width=2,line_dash="4 4", legend=legendname)
-        legendname=f"Avg Speed = {np.mean(j1_rev_stepsize2):.4f} Ontime = {model2.motorOntimeRev1[i-1]:.4f}"
-        p.line(x=j1_rev_reg2, y=j1_rev_stepsize2, color='red', line_width=2,line_dash="4 4",legend=legendname)
-        #p.circle(x=j1_rev_reg2, y=j1_rev_stepsize2,radius=1, color=mapper[colorcode],fill_color='white')
-
-        legendname=f"Avg Speed = {np.mean(j2_fwd_stepsize1):.4f} Ontime = {model1.motorOntimeFwd2[i-1]:0.4f}"
-        q.line(x=j2_fwd_reg1, y=j2_fwd_stepsize1, color='green', line_width=3, legend=legendname)
-        legendname=f"Avg Speed = {np.mean(j2_fwd_stepsize2):.4f} Ontime = {model2.motorOntimeFwd2[i-1]:0.4f}"
-        q.line(x=j2_fwd_reg2, y=j2_fwd_stepsize2, color='red', line_width=2,legend=legendname)
-        #q.circle(x=j2_fwd_reg2, y=j2_fwd_stepsize2,radius=1, color='red', fill_color='white')
+        legendname=f"Avg Speed = {np.mean(j1_fwd_stepsize2[:j1limit2]):.4f} Ontime = {model2.motorOntimeFwd1[pid-1]:.4f}"
+        p.line(x=j1_fwd_reg2[:j1limit2], y=j1_fwd_stepsize2[:j1limit2], color='red', line_width=2,legend=legendname)
+        p.circle(x=j1_fwd_reg2[:j1limit2], y=j1_fwd_stepsize2[:j1limit2],radius=1, color='red',fill_color='white')
         
-        legendname=f"Avg Speed = {np.mean(j2_rev_stepsize1):.4f} Ontime = {model1.motorOntimeRev2[i-1]:.4f}"
-        q.line(x=j2_rev_reg1, y=j2_rev_stepsize1, color='green', line_width=2,line_dash="4 4", legend=legendname)
-        legendname=f"Avg Speed = {np.mean(j2_rev_stepsize2):.4f} Ontime = {model2.motorOntimeRev2[i-1]:.4f}"
-        q.line(x=j2_rev_reg2, y=j2_rev_stepsize2, color='red', line_width=2,line_dash="4 4",legend=legendname)
-        #q.circle(x=j2_rev_reg2, y=j2_rev_stepsize2,radius=1, color='red', fill_color='white')
+        legendname=f"Avg Speed = {np.mean(j1_rev_stepsize1[:j1limit1]):.4f} Ontime = {model1.motorOntimeRev1[pid-1]:.4f}"
+        p.line(x=j1_rev_reg1[:j1limit1], y=j1_rev_stepsize1[:j1limit1], color='green', line_width=2,line_dash="4 4", legend=legendname)
+       
+        legendname=f"Avg Speed = {np.mean(j1_rev_stepsize2[:j1limit2]):.4f} Ontime = {model2.motorOntimeRev1[pid-1]:.4f}"
+        p.line(x=j1_rev_reg2[:j1limit2], y=j1_rev_stepsize2[:j1limit2], color='red', line_width=2,line_dash="4 4",legend=legendname)
+        p.circle(x=j1_rev_reg2[:j1limit2], y=j1_rev_stepsize2[:j1limit2],radius=1, color='red',fill_color='white')
 
-        colorcode = colorcode+1
+        legendname=f"Avg Speed = {np.mean(j2_fwd_stepsize1[:j2limit1]):.4f} Ontime = {model1.motorOntimeFwd2[pid-1]:0.4f}"
+        q.line(x=j2_fwd_reg1[:j2limit1], y=j2_fwd_stepsize1[:j2limit1], color='green', line_width=3, legend=legendname)
+        
+        legendname=f"Avg Speed = {np.mean(j2_fwd_stepsize2[:j2limit2]):.4f} Ontime = {model2.motorOntimeFwd2[pid-1]:0.4f}"
+        q.line(x=j2_fwd_reg2[:j2limit2], y=j2_fwd_stepsize2[:j2limit2], color='red', line_width=2,legend=legendname)
+        q.circle(x=j2_fwd_reg2[:j2limit2], y=j2_fwd_stepsize2[:j2limit2],radius=1, color='red', fill_color='white')
+        
+        legendname=f"Avg Speed = {np.mean(j2_rev_stepsize1[:j2limit1]):.4f} Ontime = {model1.motorOntimeRev2[pid-1]:.4f}"
+        q.line(x=j2_rev_reg1[:j2limit1], y=j2_rev_stepsize1[:j2limit1], color='green', line_width=2,line_dash="4 4", legend=legendname)
+        
+        legendname=f"Avg Speed = {np.mean(j2_rev_stepsize2[:j2limit2]):.4f} Ontime = {model2.motorOntimeRev2[pid-1]:.4f}"
+        q.line(x=j2_rev_reg2[:j2limit2], y=j2_rev_stepsize2[:j2limit2], color='red', line_width=2,line_dash="4 4",legend=legendname)
+        q.circle(x=j2_rev_reg2[:j2limit2], y=j2_rev_stepsize2[:j2limit2],radius=1, color='red', fill_color='white')
+
 
         #show(column(p,q))
-        export_png(column(p,q),filename=figpath+"motormap_"+str(int(pid))+".png")
+        export_png(column(p,q),filename=figPath+"motormap_"+str(int(pid))+".png")
+
+
+def main():
+    dataPath='/Volumes/Disk/Data/xml/'
+    xml1=dataPath+'motormaps_181205.xml'
+    xml2=dataPath+f'motormap20190109.xml'
+    figpath='/Volumes/Disk/Data/MotorMap/20190109/'
+    
+    brokens = [1 , 12, 39, 43, 54]
+    visibles= [e for e in range(1,58) if e not in brokens]
+    #visibles = [2,3,4]
+    
+    generateMotorMap(xml1, xml2, figpath, fiberlist=visibles)
+
 
 
 
