e80bc475d88856e786b57060c4156a518d8504ce
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2019-01-24 12:28:40 +0800

    Revise plotting function of inspectOnTime.py

--- procedures/inspectOntime.py
+++ procedures/inspectOntime.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 from scipy import stats
 
+from ics.cobraCharmer import pfi as pfiControl
 
 import xml.etree.cElementTree as ET
 
@@ -12,8 +13,7 @@ import bokeh.palettes
 from bokeh.layouts import column,gridplot
 from bokeh.models import HoverTool, ColumnDataSource, LinearColorMapper
 from bokeh.models.glyphs import Text
-from bokeh.palettes import viridis
-
+from bokeh.palettes import Category20
 from bokeh.transform import linear_cmap
 
 #output_notebook()
@@ -66,15 +66,61 @@ def readMotorMap(xml,pid):
     return j1_fwd_reg,j1_fwd_stepsize,j1_rev_reg,j1_rev_stepsize,\
            j2_fwd_reg,j2_fwd_stepsize,j2_rev_reg,j2_rev_stepsize
 
+def plotJ1OntimeSpeed(GroupIdx, dataFrame, xrange, yrange):
+
+    mapper = Category20[len(GroupIdx)]
+    TOOLS = ['pan','box_zoom','wheel_zoom', 'save' ,'reset','hover']
+
+    p = figure( tools=TOOLS, x_range=xrange, y_range=yrange,plot_height=500, plot_width=1000)
+
+    colorcode = 0
+    for i in GroupIdx:
+        legendname="Fiber "+str(i)
+        p.line(x=dataFrame['J1onTime'][dataFrame['fiberNo'] == i], y=dataFrame['J1_fwd'][dataFrame['fiberNo'] == i],\
+        color=mapper[colorcode],line_width=2,legend=legendname)
+        p.circle(x=dataFrame['J1onTime'][dataFrame['fiberNo'] == i], y=dataFrame['J1_fwd'][dataFrame['fiberNo'] == i],radius=0.3,\
+            color=mapper[colorcode],fill_color=None)
+        p.line(x=dataFrame['J1onTime'][dataFrame['fiberNo'] == i], y=dataFrame['J1_rev'][dataFrame['fiberNo'] == i],color=mapper[colorcode],line_width=2)
+        p.circle(x=dataFrame['J1onTime'][dataFrame['fiberNo'] == i], y=dataFrame['J1_rev'][dataFrame['fiberNo'] == i],radius=0.3,\
+            color=mapper[colorcode],fill_color=None)
+
+        colorcode = colorcode + 1
+
+    return p
+
+def plotJ2OntimeSpeed(GroupIdx, dataFrame, xrange, yrange):
+
+    mapper = Category20[len(GroupIdx)]
+    TOOLS = ['pan','box_zoom','wheel_zoom', 'save' ,'reset','hover']
+
+    p = figure( tools=TOOLS, x_range=xrange, y_range=yrange,plot_height=500, plot_width=1000)
+
+    colorcode = 0
+    for i in GroupIdx:
+        legendname="Fiber "+str(i)
+
+        p.line(x=dataFrame['J2onTime'][dataFrame['fiberNo'] == i], y=dataFrame['J2_fwd'][dataFrame['fiberNo'] == i],\
+        color=mapper[colorcode],line_width=2,legend=legendname)
+        p.circle(x=dataFrame['J2onTime'][dataFrame['fiberNo'] == i], y=dataFrame['J2_fwd'][dataFrame['fiberNo'] == i],radius=0.3,\
+            color=mapper[colorcode],fill_color=None)
+        p.line(x=dataFrame['J2onTime'][dataFrame['fiberNo'] == i], y=dataFrame['J2_rev'][dataFrame['fiberNo'] == i],color=mapper[colorcode],line_width=2)
+        p.circle(x=dataFrame['J2onTime'][dataFrame['fiberNo'] == i], y=dataFrame['J2_rev'][dataFrame['fiberNo'] == i],radius=0.3,\
+            color=mapper[colorcode],fill_color=None)
+
+        colorcode = colorcode + 1
+
+    return p
+
+
 
 
 def main():
-    dataPath='/Volumes/Disk/Data/xml/'
+    dataPath='/home/pfs/mhs/devel/ics_cobraCharmer/xml/'
     xml1=dataPath+'motormaps_181205.xml'
     figpath='/Volumes/Disk/Data/MotorMap/'
 
     #define the broken/good cobras
-    brokens = [1, 12, 39, 43, 54]
+    brokens = [1, 39, 43, 54]
     visibles= [e for e in range(1,58) if e not in brokens]
     badIdx = np.array(brokens) - 1
     goodIdx = np.array(visibles) - 1
@@ -102,120 +148,52 @@ def main():
         # Prepare the data path for the work
         # if not (os.path.exists(mappath)):
         #     os.makedirs(mappath)
+        pfi = pfiControl.PFI(fpgaHost='localhost', doConnect=False) #'fpga' for real device.
+        pfi.loadModel(xml2)
 
         for i in visibles:
-        
             pid=i
+            J1onTime=pfi.calibModel.motorOntimeFwd1[i-1]*1000
+            J2onTime=pfi.calibModel.motorOntimeFwd2[i-1]*1000
 
-            j1_fwd_reg1,j1_fwd_stepsize1,j1_rev_reg1,j1_rev_stepsize1,\
-                    j2_fwd_reg1,j2_fwd_stepsize1,j2_rev_reg1,j2_rev_stepsize1=readMotorMap(xml1,pid)
             j1_fwd_reg2,j1_fwd_stepsize2,j1_rev_reg2,j1_rev_stepsize2,\
                     j2_fwd_reg2,j2_fwd_stepsize2,j2_rev_reg2,j2_rev_stepsize2=readMotorMap(xml2,pid)
 
 
-            a=[tms,pid,np.mean(j1_fwd_stepsize2),np.mean(j1_rev_stepsize2),
+            a=[pid, J1onTime, J2onTime,np.mean(j1_fwd_stepsize2),np.mean(j1_rev_stepsize2),
                        np.mean(j2_fwd_stepsize2),np.mean(j2_rev_stepsize2)]
-            
             dataarray.append(a)
 
     data=np.array(dataarray)
-    d={'onTime': data[:,0], 'fiberNo': data[:,1], 
-       'J1_fwd': data[:,2], 'J1_rev': data[:,3],
-       'J2_fwd': data[:,4], 'J2_rev': data[:,5]}
+    d={'fiberNo': data[:,0],
+       'J1onTime': data[:,1], 'J2onTime': data[:,2], 
+       'J1_fwd': data[:,3], 'J1_rev': data[:,4],
+       'J2_fwd': data[:,5], 'J2_rev': data[:,6]}
     
     df = pd.DataFrame(d)
 
-
-    mapper = viridis(60)
     TOOLS = ['pan','box_zoom','wheel_zoom', 'save' ,'reset','hover']
 
-    p1 = figure( tools=TOOLS, x_range=[20, 65], y_range=[-0.3,0.3],plot_height=500, plot_width=1000)
+    p1 = plotJ1OntimeSpeed(goodGroupIdx[0], df, [20, 65], [-0.3,0.3])
+    p2 = plotJ1OntimeSpeed(goodGroupIdx[1], df, [20, 65], [-0.3,0.3])
+    p3 = plotJ1OntimeSpeed(goodGroupIdx[2], df, [20, 65], [-0.3,0.3])
+    p4 = plotJ1OntimeSpeed(goodGroupIdx[3], df, [20, 65], [-0.3,0.3])
+    p5 = plotJ1OntimeSpeed(goodGroupIdx[4], df, [20, 65], [-0.3,0.3])
+    p6 = plotJ1OntimeSpeed(goodGroupIdx[5], df, [20, 65], [-0.3,0.3])
 
-    for i in goodGroupIdx[0]:
-        legendname="Fiber "+str(i)
-        p1.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p1.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p1.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p1.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
 
-    p2 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-0.3,0.3],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[1]:
-        legendname="Fiber "+str(i)
-        p2.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p2.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p2.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p2.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-   
-    p3 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-0.3,0.3],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[2]:
-        legendname="Fiber "+str(i)
-        p3.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p3.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p3.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p3.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-
-    p4 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-0.3,0.3],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[3]:
-        legendname="Fiber "+str(i)
-        p4.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p4.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p4.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p4.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-
-    p5 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-0.3,0.3],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[4]:
-        legendname="Fiber "+str(i)
-        p5.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p5.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p5.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p5.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-
-    p6 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-0.3,0.3],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[5]:
-        legendname="Fiber "+str(i)
-        p6.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p6.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p6.line(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p6.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J1_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-    
     x = np.array([])
     y1 = np.array([])
     y2 = np.array([])
     for tms in range(25,140,10):
-        #print(np.mean(df['J1_fwd'][df['onTime']==tms].values))
         x=np.append(x,tms)
-        y1=np.append(y1,np.mean(df['J1_fwd'][df['onTime']==tms].values))
-        y2=np.append(y2,np.mean(df['J1_rev'][df['onTime']==tms].values))
+        y1=np.append(y1,np.mean(df['J1_fwd'][df['J1onTime']==tms].values))
+        y2=np.append(y2,np.mean(df['J1_rev'][df['J1onTime']==tms].values))
     
     slope, intercept, r_value, p_value, std_err = stats.linregress(x,y1)
-    #fit=np.polyfit(x, y*100, 1)
-    #print(fit)
 
     q = figure(tools=TOOLS, x_range=[20, 140], y_range=[-1,1],plot_height=500, plot_width=1000)
-    q.circle(x=df['onTime'], y=df['J1_fwd'],radius=0.3,\
+    q.circle(x=df['J1onTime'], y=df['J1_fwd'],radius=0.3,\
             color='red',fill_color=None)
     q.circle(x=x,y=y1, radius=0.5, color='blue')
     legendtext=f'Y={slope:.8f}X{intercept:.2f}'
@@ -223,90 +201,24 @@ def main():
 
     slope, intercept, r_value, p_value, std_err = stats.linregress(x,y2)
     #fit=np.polyfit(x, y*100, 1)
-    q.circle(x=df['onTime'], y=df['J1_rev'],radius=0.3,\
+    q.circle(x=df['J1onTime'], y=df['J1_rev'],radius=0.3,\
             color='green',fill_color=None)
     q.circle(x=x,y=y2, radius=0.5, color='#3B0F6F')
     legendtext=f'Y={slope:.8f}X+{intercept:.2f}'
     q.line(x=x, y=x*slope+intercept,color='#3B0F6F',line_width=3, legend=legendtext)
 
     output_file("theta.html")
-    #show(column(p1,p2,p3,p4,p5,p6,q))
     save(column(p1,p2,p3,p4,p5,p6,q), filename="theta.html", \
         title='Theta On-time')
 
 #-------------------------------------------
+    p1 = plotJ2OntimeSpeed(goodGroupIdx[0], df, [20, 65], [-1,1])
+    p2 = plotJ2OntimeSpeed(goodGroupIdx[1], df, [20, 65], [-1,1])
+    p3 = plotJ2OntimeSpeed(goodGroupIdx[2], df, [20, 65], [-1,1])
+    p4 = plotJ2OntimeSpeed(goodGroupIdx[3], df, [20, 65], [-1,1])
+    p5 = plotJ2OntimeSpeed(goodGroupIdx[4], df, [20, 65], [-1,1])
+    p6 = plotJ2OntimeSpeed(goodGroupIdx[5], df, [20, 65], [-1,1])
 
-    p1 = figure( tools=TOOLS, x_range=[20, 65], y_range=[-1,1],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[0]:
-        legendname="Fiber "+str(i)
-        p1.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p1.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p1.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p1.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-
-    p2 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-1,1],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[1]:
-        legendname="Fiber "+str(i)
-        p2.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p2.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p2.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p2.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-   
-    p3 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-1,1],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[2]:
-        legendname="Fiber "+str(i)
-        p3.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p3.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p3.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p3.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-
-    p4 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-1,1],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[3]:
-        legendname="Fiber "+str(i)
-        p4.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p4.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p4.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p4.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-
-    p5 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-1,1],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[4]:
-        legendname="Fiber "+str(i)
-        p5.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p5.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p5.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p5.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-
-    p6 = figure(tools=TOOLS, x_range=[20, 65], y_range=[-1,1],plot_height=500, plot_width=1000)
-
-    for i in goodGroupIdx[5]:
-        legendname="Fiber "+str(i)
-        p6.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],\
-        color=mapper[i],line_width=1,legend=legendname)
-        p6.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_fwd'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
-        p6.line(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],color=mapper[i],line_width=1)
-        p6.circle(x=df['onTime'][df['fiberNo'] == i], y=df['J2_rev'][df['fiberNo'] == i],radius=0.3,\
-            color=mapper[i],fill_color=None)
     
     x = np.array([])
     y1 = np.array([])
@@ -314,15 +226,15 @@ def main():
     for tms in range(25,140,10):
         #print(np.mean(df['J2_fwd'][df['onTime']==tms].values))
         x=np.append(x,tms)
-        y1=np.append(y1,np.median(df['J2_fwd'][df['onTime']==tms].values))
-        y2=np.append(y2,np.median(df['J2_rev'][df['onTime']==tms].values))
+        y1=np.append(y1,np.median(df['J2_fwd'][df['J2onTime']==tms].values))
+        y2=np.append(y2,np.median(df['J2_rev'][df['J2onTime']==tms].values))
     
     slope, intercept, r_value, p_value, std_err = stats.linregress(x[:8],y1[:8])
     #fit=np.polyfit(x, y*100, 1)
     #print(fit)
 
     q = figure(tools=TOOLS, x_range=[20, 140], y_range=[-1.5,1.5],plot_height=500, plot_width=1000)
-    q.circle(x=df['onTime'], y=df['J2_fwd'],radius=0.3,\
+    q.circle(x=df['J2onTime'], y=df['J2_fwd'],radius=0.3,\
             color='red',fill_color=None)
     q.circle(x=x,y=y1, radius=0.5, color='blue')
     legendtext=f'Y={slope:.8f}X{intercept:.2f}'
@@ -330,7 +242,7 @@ def main():
 
     slope, intercept, r_value, p_value, std_err = stats.linregress(x[:8],y2[:8])
     #fit=np.polyfit(x, y*100, 1)
-    q.circle(x=df['onTime'], y=df['J2_rev'],radius=0.3,\
+    q.circle(x=df['J2onTime'], y=df['J2_rev'],radius=0.3,\
             color='green',fill_color=None)
     q.circle(x=x,y=y2, radius=0.5, color='#3B0F6F')
     legendtext=f'Y={slope:.8f}X+{intercept:.2f}'
