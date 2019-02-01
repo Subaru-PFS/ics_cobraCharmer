5a13a60701d4f37a62c72e8e8ff0196c18dd42ae
Craig Loomis <cloomis@astro.princeton.edu>
2019-02-01 09:48:10 -0800

    Switch to controlling modules and always update internal model.

--- procedures/calibrateMotorFrequencies.py
+++ procedures/calibrateMotorFrequencies.py
@@ -1,19 +1,20 @@
-import ics.cobraCharmer.pfi as pfiModule
-import ics.cobraCharmer.pfiDesign as pfiDesign
+from ics.cobraCharmer import pfi as pfiModule
+from ics.cobraCharmer import pfiDesign
+from ics.cobraCharmer.utils import fileManager
 
-def calibrateMotorFrequencies(pfi=None, boards=None, updateModel=True, fpgaHost='localhost'):
+def calibrateMotorFrequencies(pfi, modules=None, updateModel=True):
     """ Calibrate cobras.
 
     Args
     ----
     pfi : PFI instance
        A collection of 1+ cobra modules, with loaded model.
-    boards : a list of (module, board) ids.
-       If set, limit calibration to those boards.
+    modules : a list of module numbers.
+       If set, limit calibration to those modules.
     updateModel : bool
-       Update
+       Whether to update the pfi's internal model
 
-    We limit calibrations to individual full boards. The _calibration_
+    We limit calibrations to individual full modules. The _calibration_
     step will run on individual cobras, but readback of measured
     frequencies happens to full boards. Avoid confusion by limiting
     this step.
@@ -21,18 +22,15 @@ def calibrateMotorFrequencies(pfi=None, boards=None, updateModel=True, fpgaHost=
 
     """
 
-    if pfi is None:
-        pfi = pfiModule.PFI(fpgaHost=fpgaHost, doLoadModel=False)
-
-    if boards is not None:
+    if modules is not None:
         cobras = []
-        for b in boards:
-            mod, brd = b
-            c1 = pfi.allocateCobraBoard(mod, brd)
+        for m in modules:
+            c1 = pfi.allocateCobraModule(m)
             cobras.extend(c1)
     else:
         cobras = pfi.getAllConnectedCobras()
-        boards = {(c.module, c.board) for c in cobras}
+
+    boards = {(c.module, c.board) for c in cobras}
 
     pfi.calibrate(cobras)
     for b in boards:
@@ -48,32 +46,29 @@ def main(args=None):
 
     import argparse
 
-    parser = argparse.ArgumentParser('calibrateMotorFrequencies')
+    parser = argparse.ArgumentParser('calibrateMotorFrequencies', add_help=True)
+    parser.add_argument('moduleName', type=str,
+                        help='the name of the module (e.g. "SC03", "Spare1", or "PFI")')
+
     parser.add_argument('--fpgaHost', type=str, default='localhost',
                         help='connect to the given FPGA host instead of the simulator.')
-    parser.add_argument('--modelName', type=str, default='',
+    parser.add_argument('--modelName', type=str, default=None,
                         help='load the given PFI model before calibrating.')
-    parser.add_argument('--updateModel', action='store_true',
-                        help='Update the loaded model. Requires --modelName')
+    parser.add_argument('--module', type=int, default=0,
+                        help='calibrate the given module. Or all.')
     parser.add_argument('--saveModelFile', type=str, default='',
-                        help='save the updated model in the given file. Requires --updateModel')
+                        help='save the updated model in the given file.')
 
     opts = parser.parse_args(args)
 
-    if opts.updateModel and not opts.modelName:
-        parser.error('--updateModel requires --modelName')
+    output = fileManager.ProcedureDirectory(opts.moduleName, experimentName='calibrate')
 
-    if opts.saveModelFile and not opts.updateModel:
-        parser.error('--saveModelFile requires --updatemodel')
+    pfi = pfiModule.PFI(fpgaHost=opts.fpgaHost, logDir=output.logDir,
+                        doLoadModel=False)
+    pfi.loadModel(opts.modelName)
 
-    if opts.modelName:
-        pfi = pfiModule.PFI(fpgaHost=opts.fpgaHost, doLoadModel=False)
-        pfi.loadModel(opts.modelName)
-    else:
-        pfi = None
-
-    pfi = calibrateMotorFrequencies(pfi=pfi, fpgaHost=opts.fpgaHost,
-                                    updateModel=opts.updateModel)
+    pfi = calibrateMotorFrequencies(pfi=pfi, output=output,
+                                    modules=[opts.module] if opts.module != 0 else None0
 
     if opts.saveModelFile:
         pfi.calibModel.createCalibrationFile(opts.saveModelFile)
