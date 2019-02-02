7d92e4d6893393ff2f69deef7d8a265731b4b0ef
Craig Loomis <cloomis@astro.princeton.edu>
2019-02-01 18:09:17 -0800

    Extract some common procedure-only routines.

---
+++ procedures/utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def lazyIdentification(centers, spots, radii=None):
+    n = len(centers)
+    if radii is not None and len(radii) != n:
+        raise RuntimeError("number of centers must match number of radii")
+    ans = np.empty(n, dtype=int)
+    for i in range(n):
+        dist = np.absolute(spots - centers[i])
+        j = np.argmin(dist)
+        if radii is not None and np.absolute(centers[i] - spots[j]) > radii[i]:
+            ans[i] = -1
+        else:
+            ans[i] = j
+    return ans
+
+def circle_fitting(p):
+    x = np.real(p)
+    y = np.imag(p)
+    m = np.vstack([x, y, np.ones(len(p))]).T
+    n = np.array(x*x + y*y)
+    a, b, c = np.linalg.lstsq(m, n, rcond=None)[0]
+    return a/2, b/2, np.sqrt(c+(a*a+b*b)/4)
