f2a39150c8899ad13b90d7b5539ee5fec57e22d5
Chih-Yi Wen <wency@asiaa.sinica.edu.tw>
2019-05-28 16:09:59 +0800

    Add tools for single module test

---
+++ procedures/moduleTest/moduleGUI.py
@@ -0,0 +1,178 @@
+from PyQt5.QtWidgets import (
+    QPushButton, QMainWindow, QHBoxLayout, QLineEdit, QSplitter,
+    QSizePolicy, QLabel, QCheckBox, QVBoxLayout, QWidget, QApplication)
+from PyQt5.QtGui import QIntValidator
+from PyQt5.QtCore import Qt
+import sys
+import os
+import numpy as np
+from moduleTest import ModuleTest
+
+active_cobras = np.full(57, False)
+bad_cobras = np.full(57, False)
+IP = '128.149.77.24'
+#IP = '133.40.164.251'
+
+class CobraButton(QPushButton):
+    def __init__(self, idx):
+        super().__init__()
+        self.idx = idx
+        self.setText(str(idx+1))
+        self.setCheckable(True)
+        self.setMaximumWidth(20)
+
+    def mousePressEvent(self, QMouseEvent):
+        if QMouseEvent.button() == Qt.LeftButton:
+            active_cobras[self.idx] = not active_cobras[self.idx]
+            super().mousePressEvent(QMouseEvent)
+        elif QMouseEvent.button() == Qt.RightButton:
+            bad_cobras[self.idx] = not bad_cobras[self.idx]
+            if bad_cobras[self.idx]:
+                self.setStyleSheet('background-color: #00FF0000')
+            else:
+                self.setStyleSheet('background-color: None')
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.func = None
+        self.pfi = None
+
+        self.setWindowTitle('Cobra controller')
+        self.statusBar().showMessage('Please load XML file')
+
+        block1 = QHBoxLayout()
+        self.xml = QLineEdit()
+        self.xml.setMinimumWidth(240)
+        block1.addWidget(self.xml)
+        self.btn_xml = QPushButton('Load XML file')
+        self.btn_xml.clicked.connect(self.click_load)
+        block1.addWidget(self.btn_xml)
+        block1.addWidget(QSplitter(Qt.Vertical), QSizePolicy.Expanding)
+        btn = QPushButton('Move Up/Down')
+        btn.clicked.connect(self.move_up_down)
+        block1.addWidget(btn)
+
+        block2 = QHBoxLayout()
+        btn = QPushButton('Forward')
+        block2.addWidget(btn, QSizePolicy.Expanding)
+        btn.clicked.connect(lambda: self.click_go(1))
+        onlyInt = QIntValidator()
+        block2.addWidget(QLabel('theta:'))
+        self.theta = QLineEdit('0')
+        self.theta.setValidator(onlyInt)
+        block2.addWidget(self.theta)
+        block2.addWidget(QLabel('phi:'))
+        self.phi = QLineEdit('0')
+        self.phi.setValidator(onlyInt)
+        block2.addWidget(self.phi)
+        btn = QPushButton('Reverse')
+        block2.addWidget(btn, QSizePolicy.Expanding)
+        btn.clicked.connect(lambda: self.click_go(-1))
+
+        block3 = QHBoxLayout()
+        btn_odd = QPushButton('Odd')
+        btn_odd.setCheckable(True)
+        btn_odd.clicked[bool].connect(self.click_odd)
+        block3.addWidget(btn_odd)
+        block4 = QHBoxLayout()
+        btn_even = QPushButton('Even')
+        btn_even.setCheckable(True)
+        btn_even.clicked[bool].connect(self.click_even)
+        block4.addWidget(btn_even)
+        self.btn_cobras = []
+        for idx in range(57):
+            btn_c = CobraButton(idx)
+            self.btn_cobras.append(btn_c)
+        for idx in range(56, -1, -1):
+            if idx % 2 == 0:
+                block3.addWidget(self.btn_cobras[idx], QSizePolicy.Minimum)
+            else:
+                block4.addWidget(self.btn_cobras[idx], QSizePolicy.Minimum)
+        btn_c = QPushButton()
+        btn_c.setMaximumWidth(20)
+        block4.addWidget(btn_c)
+
+        block5 = QHBoxLayout()
+        block5.addWidget(QLabel('Right click to mark bad cobras'), QSizePolicy.Expanding)
+        self.cb_use_bad = QCheckBox('bad cobras')
+        block5.addWidget(self.cb_use_bad)
+        self.btn_speed = QPushButton('Fast')
+        self.btn_speed.setCheckable(True)
+        self.btn_speed.clicked[bool].connect(self.click_speed)
+        block5.addWidget(self.btn_speed)
+
+        layout = QVBoxLayout()
+        layout.addLayout(block1)
+        layout.addWidget(QSplitter())
+        layout.addLayout(block2)
+        layout.addLayout(block5)
+        layout.addLayout(block3)
+        layout.addLayout(block4)
+
+        cw = QWidget()
+        cw.setLayout(layout)
+        self.setCentralWidget(cw)
+
+    def click_speed(self, pressed):
+        if pressed:
+            self.btn_speed.setText('Slow')
+        else:
+            self.btn_speed.setText('Fast')
+
+    def click_go(self, direction=1):
+        if self.func is None:
+            self.statusBar().showMessage('Load XML file first!')
+            return
+        cIdx = [idx for idx, c in enumerate(active_cobras) if c]
+        if not self.cb_use_bad.isChecked():
+            cIdx = [idx for idx in cIdx if not bad_cobras[idx]]
+        if len(cIdx) <= 0:
+            self.statusBar().showMessage('No cobras selected!')
+            return
+
+        use_fast = not self.btn_speed.isChecked()
+        cobras = pfiUtility.getCobras(cIdx)
+        theta_steps = int(self.theta.text()) * direction
+        phi_steps = int(self.phi.text()) * direction
+        self.statusBar().showMessage('Start to moving cobra...')
+        self.pfi.moveAllSteps(cobras, theta_steps, phi_steps, thetaFast=use_fast, phiFast=use_fast)
+        self.statusBar().showMessage('Moving cobra succeed!')
+
+    def click_odd(self, pressed):
+        for idx in range(0, 57, 2):
+            self.btn_cobras[idx].setChecked(pressed)
+            active_cobras[idx] = pressed
+
+    def click_even(self, pressed):
+        for idx in range(1, 57, 2):
+            self.btn_cobras[idx].setChecked(pressed)
+            active_cobras[idx] = pressed
+
+    def click_load(self):
+        if len(self.xml.text()) <= 0:
+            self.statusBar().showMessage(f"Error: enter XML filename!")
+            return
+        xml = '../xml/' + self.xml.text()
+        if not os.path.exists(xml):
+            self.statusBar().showMessage(f"Error: {xml} not presented!")
+            return
+
+        if self.func is None:
+            self.func = ModuleTest(IP, xml)
+            self.pfi = self.func.pfi
+
+        self.btn_xml.setStyleSheet('background-color: green')
+
+    def move_up_down(self):
+        self.statusBar().showMessage(f"Function not implemented!")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
