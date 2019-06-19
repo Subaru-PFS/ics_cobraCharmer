1533bf6c843f6f3b4c7dcaf17b065cb92918929e
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2019-06-19 13:10:58 +0800

    Fixing numpy issue by adding version check

--- procedures/moduleTest/moduleAnalyze.py
+++ procedures/moduleTest/moduleAnalyze.py
@@ -5,6 +5,8 @@ from astropy.io import fits
 import sep
 from copy import deepcopy
 from ics.cobraCharmer import pfiDesign
+from scipy import optimize
+
 
 class moduleAnalyze():
     def __init__(self, xml, brokens=None, camSplit=26):
@@ -821,7 +823,31 @@ def lazyIdentification(centers, spots, radii=None):
 def circle_fitting(p):
     x = np.real(p)
     y = np.imag(p)
-    m = np.vstack([x, y, np.ones(len(p))]).T
-    n = np.array(x*x + y*y)
-    a, b, c = np.linalg.lstsq(m, n, rcond=None)[0]
-    return a/2, b/2, np.sqrt(c+(a*a+b*b)/4)
+
+    # If numpy version is greater than 1.14, use scipy instead
+    if float(np.__version__.split('.')[1]) > 14:
+        x_m = np.mean(x)
+        y_m = np.mean(y)
+        center_estimate = x_m, y_m
+        center, ier = optimize.leastsq(f, center_estimate, args=(x,y))
+        xc, yc = center
+        Ri       = calc_R(x, y, *center)
+        R        = Ri.mean()
+        return xc, yc, R  
+    else:
+        m = np.vstack([x, y, np.ones(len(p))]).T
+        n = np.array(x*x + y*y)
+        a, b, c = np.linalg.lstsq(m, n, rcond=None)[0]
+        return a/2, b/2, np.sqrt(c+(a*a+b*b)/4)
+
+
+
+def calc_R(x,y, xc, yc):
+    """ calculate the distance of each 2D points from the center (xc, yc) """
+    return np.sqrt((x-xc)**2 + (y-yc)**2)
+
+def f(c, x, y):
+    """ calculate the algebraic distance between the data points and the mean circle centered at c=(xc, yc) """
+    Ri = calc_R(x, y, *c)
+    return Ri - Ri.mean()
+
