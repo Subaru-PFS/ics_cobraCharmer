d2c91c0e15bd09287549467075ee440546b8b343
Craig Loomis <cloomis@astro.princeton.edu>
2019-09-18 16:09:01 -0400

    Support clearing moves and surviving undefined queries

--- python/ics/cobraCharmer/fpgaState.py
+++ python/ics/cobraCharmer/fpgaState.py
@@ -10,21 +10,35 @@ class FpgaState(object):
         """ Return an indexable id for a cobra. """
         return (cobra.module, cobra.cobraNum)
 
+    def clearMoves(self):
+        """ Declare a new move, by setting all known cobras to not have move info.
+
+        The expectation is that all _moving_ cobras will be set with .runCobra
+        """
+        for cm in self.cobraMoves.values():
+            cm['thetaSteps'] = np.nan
+            cm['phiSteps'] = np.nan
+
+    def _blankMove(self):
+        cobraInfo = dict()
+        cobraInfo['thetaSteps'] = np.nan
+        cobraInfo['phiSteps'] = np.nan
+        cobraInfo['thetaOntime'] = np.nan
+        cobraInfo['phiOntime'] = np.nan
+
+        return cobraInfo
+
     def runCobra(self, cobraBlock):
         moveParams = cobraBlock.p
         thetaEnabled, phiEnabled = moveParams.en
 
-        cobraInfo = dict()
+        cobraInfo = self._blankMove()
         if thetaEnabled:
             cobraInfo['thetaSteps'] = (moveParams.steps[0] *
                                        (-1 if moveParams.dir[0] == 'ccw' else 1))
-        else:
-            cobraInfo['thetaSteps'] = np.nan
         if phiEnabled:
             cobraInfo['phiSteps'] = (moveParams.steps[1] *
                                      (-1 if moveParams.dir[1] == 'ccw' else 1))
-        else:
-            cobraInfo['phiSteps'] = np.nan
 
         cobraInfo['thetaOntime'] = moveParams.pulses[0] / 1000
         cobraInfo['phiOntime'] = moveParams.pulses[1] / 1000
@@ -35,6 +49,9 @@ class FpgaState(object):
         """ Return the last move we know about for a given cobra """
 
         cobraId = self.cobraId(cobraBlock)
-        return self.cobraMoves[cobraId]
+        try:
+            return self.cobraMoves[cobraId]
+        except KeyError:
+            return self._blankMove()
 
 fpgaState = FpgaState()
