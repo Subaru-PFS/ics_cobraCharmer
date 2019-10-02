156a9725bb7ec8ef8220a79f2d45fdbea71ba0de
Craig Loomis <cloomis@astro.princeton.edu>
2019-10-02 03:14:05 -0400

    Add validate{Phi,Theta}Limits()

--- python/ics/cobraCharmer/pfiDesign.py
+++ python/ics/cobraCharmer/pfiDesign.py
@@ -47,6 +47,8 @@ class PFIDesign():
         PFIDesign.loadPFI().
         """
 
+        self.logger = logging.getLogger('pfiDesign')
+
         if fileName is None:
             return
         import warnings
@@ -737,4 +739,35 @@ class PFIDesign():
 
         # Save the new XML calibration file
         newXmlTree.write(outputFileName, encoding="UTF-8", xml_declaration=True)
-        logging.info(f'wrote pfiDesign file for {self.nCobras} cobras and name={name} to {str(outputFileName)}')
+        self.logger.info(f'wrote pfiDesign file for {self.nCobras} cobras and name={name} to {str(outputFileName)}')
+
+    def validatePhiLimits(self, rangeOnly=True):
+        """ Confirm that the phi limits are sane. """
+
+        phiRange = self.phiOut - self.phiIn
+        phiRange[phiRange<0] += 2*np.pi
+        phiRange[phiRange>=2*np.pi] -= 2*np.pi
+
+        if not rangeOnly:
+            raise NotImplementedError('not checking phi limit _positions_ yet.')
+
+        duds = phiRange < np.pi
+
+        for cidx in np.where(duds)[0]:
+            with np.printoptions(precision=2, suppress=True):
+                self.logger.warn(f'phi limits bad: mod={self.moduleIds[cidx]} pos={self.positionerIds[cidx]} '
+                                 f'CCW={np.rad2deg(self.phiIn[cidx])} CW={np.rad2deg(self.phiOut[cidx])}')
+        return ~duds
+
+    def validateThetaLimits(self):
+        """ Confirm that the theta limits are sane. """
+
+        thetaRange = self.tht1 - self.tht0
+        thetaRange[thetaRange<np.pi/4] += 2*np.pi
+        duds = thetaRange < np.deg2rad(370)
+
+        for cidx in np.where(duds)[0]:
+            with np.printoptions(precision=2, suppress=True):
+                self.logger.warn(f'theta limits bad: mod={self.moduleIds[cidx]} pos={self.positionerIds[cidx]} '
+                                 f'range={np.rad2deg(thetaRange[cidx])}')
+        return ~duds
