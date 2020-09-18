65c28d9fbc95808ab888a74034ac0b90e19c9beb
Craig Loomis <cloomis@astro.princeton.edu>
2020-09-18 04:32:39 -0400

    Load the PFI slot-by-slot, with each slot getting a named module.

--- python/ics/cobraCharmer/pfi.py
+++ python/ics/cobraCharmer/pfi.py
@@ -33,18 +33,49 @@ class PFI(object):
             self.loadCobras()
 
     def __str__(self):
-        return f"PFI(ncobras={len(self.cobras)}, modules={sorted(self.modules)})"
+        return f"PFI(version={self.version} ncobras={len(self.cobras)}, modules={self.modules})"
 
     def loadCobras(self, configPath=None):
+        """
+        Load all the cobras defined in the pfi.yaml config file.
+
+        The `pfi.yaml` file has a `modules` list, binding named
+        cobra modules ("SC01", etc) to module ids in the PFI focal plane.
+
+        For some better tracability and error checking, we save both
+        the module name and the module slot number in the cobra
+        object. If we detect that a module has been moved, update its
+        cobras and persist them.
+
+        PFI slots are filled consecutively, starting at slot 1. If a
+        slot's module name is "None", do not fill that slot, and
+        continue with the next one.
+        """
+
         if configPath is None:
             configPath = self.butler.getPath('pfi', version=self.version)
 
         cfg = self.butler.getFromPath('pfi', configPath)
-        self.modules = set(cfg['modules'])
+        self.modules = cfg['modules']
 
-        for m in sorted(self.modules):
+        for m_i, m in enumerate(self.modules):
+            moduleNum = m_i + 1
+            if m is None or m == "None":
+                self.logger.warn(f'skipping empty module slot {moduleNum}')
+                continue
+            self.logger.info(f'loading module {m} in PFI slot {moduleNum}')
+
             idDict = dict(moduleName=m)
+            assert m is not None
             for c_i in range(1, self.COBRAS_PER_MODULE+1):
                 idDict['cobraInModule'] = c_i
                 cobra = self.butler.get('cobraGeometry', idDict)
+                lastModuleNum = cobra.setModuleNum(moduleNum)
+                if lastModuleNum != moduleNum:
+                    if c_i == 1:
+                        logging.warn(f'moving module {cobra.moduleName} from slot {lastModuleNum} to {moduleNum}')
+                    cobra.setModuleNum(moduleNum)
+                    assert cobra.moduleName is not None
+                    self.butler.put(cobra, 'cobraGeometry', dict(moduleName=cobra.moduleName,
+                                                                 cobraInModule=cobra.cobraNum))
                 self.cobras[cobra.cobraId] = cobra
