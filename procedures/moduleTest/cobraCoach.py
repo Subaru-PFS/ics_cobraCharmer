a42aa405dbb510f055b542bd7961efe10a7ae335
Chi-Hung Yan <chyan@asiaa.sinica.edu.tw>
2022-11-19 22:21:19 -1000

    Adding message

--- procedures/moduleTest/cobraCoach.py
+++ procedures/moduleTest/cobraCoach.py
@@ -1074,9 +1074,10 @@ class CobraCoach():
             # Here we need to think about how to deal with it. 
             # There are dots, so we may wanto to skip this
             if noMCS is True:
+                self.logger.info('noMCS flag is passed, set angles instead of exposure.')
                 thetaHome = ((self.calibModel.tht1 - self.calibModel.tht0 + np.pi)
                               % (np.pi*2) + np.pi)
-                self.setCurrentAngles(self.allCobras, thetaAngles=thetaHome, phiAngles=0)             
+                self.setCurrentAngles(self.allCobras[cIds], thetaAngles=thetaHome[cIds], phiAngles=0)             
             else:
                 self.cobraInfo['position'][self.visibleIdx] = self.exposeAndExtractPositions(dbMatch = False)[self.visibleIdx]
 
